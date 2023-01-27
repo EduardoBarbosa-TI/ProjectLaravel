6c4656d72bea77820c1ee78821874f0a4f3dfb45
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-01-27 17:33:00 -0300

    Criando front-end da aplicação de temporadas e episódios

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
+use App\Models\Season;
 use App\Models\Series;
+use App\Models\Episode;
 use Illuminate\Http\Request;
 
 
 class SeriesController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $series = Series::all();
         $mensagemSucesso = session('mensagem.sucesso');
-       
+    
         return view('series.index')->with('series', $series)
             ->with('mensagemSucesso', $mensagemSucesso);
     }
@@ -25,9 +28,30 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request)
     {
         $serie = Series::create($request->all());
+        $seasons = [];
+        for ($i = 1; $i <= $request->seasonsQty; $i++) {
+            $seasons[] = [
+                'series_id' => $serie->id,
+                'number' => $i,
+            ];
+        }
+        Season::insert($seasons);
+
+        $episodes = [];
+        foreach($serie->seasons as $season){
+            for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
+                $episodes[] = [
+                    'season_id' => $season->id,
+                    'number' => $j
+                ];
+            } 
+        }
+
+        Episode::insert($episodes);
 
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+        
     }
     public function destroy(Series $series)
     {
